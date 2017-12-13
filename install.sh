6a6a744719d0bbe37975629932e7f2e40de7ed02
Projetomoni <34343426+Projetomoni@users.noreply.github.com>
2017-12-13 18:05:21 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ apt-get update && apt-get install apache2 -y && apt-get install dialog -y || fal
 a2enmod cgid
 cp /etc/apache2/conf-enabled/charset.conf charset.conf.bkp
 echo "AddDefaultCharset UTF-8" >> /etc/apache2/conf-enabled/charset.conf
+service apache2 restart
 systemctl restart apache2
 cp -R /var/www/html /var/www/html.bkp
 cp -R /usr/lib/cgi-bin /usr/lib/cgi-bin.bkp
