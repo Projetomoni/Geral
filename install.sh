575bfec047cfe22db3f60e0ea68b3260fb665c7e
Projetomoni <34343426+Projetomoni@users.noreply.github.com>
2017-12-13 14:41:56 -0200

    NOVO

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 falhou(){
         case $1 in
         instalacao)
-                echo "O programa falhou na instalação"
+                echo "O programa falhou na instalacao"
                 exit 0 ;;
         *)
-                echo "o programa falhou. Causa desconhecida"
+                echo "O programa falhou. Causa desconhecida"
                 exit 0 ;;
         esac
 }
-apt-get update && apt-get install apache2 && apt-get install dialog || falhou instalacao
+apt-get update && apt-get install apache2 dialog -y || falhou instalacao
 a2enmod cgid
 cp /etc/apache2/conf-enabled/charset.conf charset.conf.bkp
 echo "AddDefaultCharset UTF-8" >> /etc/apache2/conf-enabled/charset.conf
 $(systemctl restart apache2)
-./red.sh
+cp -R /var/www/html /var/www/html.bkp
+cp -R /usr/lib/cgi-bin /usr/lib/cgi-bin.bkp
+cp -R ./html /var/www
+cp -R ./cgi-bin /usr/lib
+chmod 777 *
